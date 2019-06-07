c2292c82bc9769a3bf8cdd89fadaba11fd812077
Andy Hardy <38298019+ahardy42@users.noreply.github.com>
2019-06-07 13:44:26 -0600

    Update javascript.js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -104,7 +104,7 @@ $(document).ready(function() {
      // watching for a location event
      map.on("locationfound", async function(event) {
         // add a small marker to the location
-        map.flyTo(event.latlng, 12);
+        map.zoom(12).flyTo(event.latlng);
         locationIcon.setLatLng(event.latlng).addTo(map);
         // get the share that is closest
         if (!closestShare) {
